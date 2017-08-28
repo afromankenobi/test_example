114a1cb21a375a014fe7447a1a7d791300f1d430
Jorge Vargas <jorge.vargas@clickfono.com>
2017-08-28 14:54:32 -0300

    routes for welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/factorial'
+  get 'welcome/number_to_words'
+  get 'welcome/data_model'
+  get 'welcome/oop'
+  
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
