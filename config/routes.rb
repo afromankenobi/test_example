a5571b7049158ba69c8f1863f118b1f8bb3a9053
Jorge Vargas <jorge.vargas@clickfono.com>
2017-08-29 00:52:11 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  resources :evaluations
+  resources :tests
+  resources :courses
+  resources :members
+
   get 'welcome/index'
   get 'welcome/factorial'
   get 'welcome/number_to_words'
   get 'welcome/data_model'
   get 'welcome/oop'
-  
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
