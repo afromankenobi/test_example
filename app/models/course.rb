8d5ff3fb43b98f577d747db982b24a5dca690aeb
Jorge Vargas <jorge.vargas@clickfono.com>
2017-08-29 01:18:58 -0300

    Show a decent title

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :members, through: :participations
   has_many :tests
+
+  def to_s
+    name.titleize
+  end
 end
