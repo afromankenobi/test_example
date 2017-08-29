e7cc4e37682de24f907fb3cb5ed8d334815834b4
Jorge Vargas <jorge.vargas@clickfono.com>
2017-08-29 01:55:02 -0300

    participations for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -69,6 +69,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:name)
+      params.require(:course).permit(:name, member_ids: [])
     end
 end
